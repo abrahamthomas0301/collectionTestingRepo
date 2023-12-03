ee7267052568e4e0c8776b7b55311a4be72b7857
abrahamthomas0301 <abrahamthomas0301@gmail.com>
2023-12-03 10:56:03 +0530

    changes to display and delete and exit

--- src/main/java/testing/ArrayListOperations.java
+++ src/main/java/testing/ArrayListOperations.java
@@ -4,19 +4,22 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ArrayListOperations {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<String> userlist=new ArrayList();
+		while(true) {
 		System.out.println("Arraylist operations");
 		System.out.println("1. select to enter data");
 		System.out.println("2. select to display data");
+		System.out.println("3. exit");
 		 int userinput;
 	       Scanner input = new Scanner(System.in);
 	       userinput=input.nextInt();
+	       
 	       switch(userinput) {
 	       case 1:
 	    	   System.out.println("enter the data forr list:");
+	    	   input.nextLine();
 	    	   String inputString=input.nextLine();
 	    	   userlist.add(inputString);
 	    	   System.out.println(inputString.length());
@@ -25,11 +28,14 @@ public class ArrayListOperations {
 	    	   System.out.println("Entered data");
 	    	   userlist.forEach(value->System.out.println(value));
 	    	   break;
+	    	case 3:
+	    	   userlist=null;
+	    	   return ;
 	        default:
 	    	   System.out.println("invalid");
 	    	   break;
 	       }
 
 	}
-
+	}
 }
