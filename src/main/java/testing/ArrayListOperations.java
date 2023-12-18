28c60c3538e2143d69172f67adcc9dae67b187c4
abrahamthomas0301 <abrahamthomas0301@gmail.com>
2023-12-18 19:24:46 +0530

    commiting chnages for delete

--- src/main/java/testing/ArrayListOperations.java
+++ src/main/java/testing/ArrayListOperations.java
@@ -11,24 +11,42 @@ public class ArrayListOperations {
 		System.out.println("Arraylist operations");
 		System.out.println("1. select to enter data");
 		System.out.println("2. select to display data");
-		System.out.println("3. exit");
-		 int userinput;
+		System.out.println("3. select to delete data");
+		System.out.println("E. exit");
+		 String userinput;
+		 
 	       Scanner input = new Scanner(System.in);
-	       userinput=input.nextInt();
 	       
+	       userinput=input.nextLine();
+	      // input.nextLine();
 	       switch(userinput) {
-	       case 1:
+	       case "1":
 	    	   System.out.println("enter the data forr list:");
-	    	   input.nextLine();
+	    	   
 	    	   String inputString=input.nextLine();
 	    	   userlist.add(inputString);
 	    	   System.out.println(inputString.length());
 	    	   break;
-	       case 2:
+	       case "2":
 	    	   System.out.println("Entered data");
 	    	   userlist.forEach(value->System.out.println(value));
 	    	   break;
-	    	case 3:
+	       case "3":
+	    	   System.out.println("Current data");
+	    	   int index=0;
+	    	  for(String s:userlist) {
+	    		  System.out.println(index +":"+s);
+	    		  index++;
+	    	  }
+	    	  System.out.println("select index to remove");
+	    	  int indexxtodelete=input.nextInt();
+	    	  if(indexxtodelete>=0 && indexxtodelete<userlist.size()) {
+	    		  userlist.remove(indexxtodelete);
+	    	  }
+	    	  System.out.println("Entered data");
+	    	   userlist.forEach(value->System.out.println(value));
+	    	   break;
+	    	case "E":
 	    	   userlist=null;
 	    	   return ;
 	        default:
